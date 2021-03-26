872a204443c70f000c0f38212852e23ddfe1199c
Colin Bassett <ccwbassett@gmail.com>
2021-03-25 22:27:45 -0700

    passing, added POST new order, fixed updated all tests.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,6 @@ const setupTest = require('../data/setup-test-db');
 const request = require('supertest');
 const app = require('../lib/app');
 
-const allOrdersResult = [{"order_id":1,"coffee_type":"Ethopian","quantity":5,"order_date":"2021-03-25T07:00:00.000Z","subscription":2,"linked_customer_id":1},{"order_id":2,"coffee_type":"Honduras","quantity":10,"order_date":"2021-03-25T07:00:00.000Z","subscription":3,"linked_customer_id":1},{"order_id":3,"coffee_type":"Brazil","quantity":15,"order_date":"2021-03-25T07:00:00.000Z","subscription":4,"linked_customer_id":2},{"order_id":4,"coffee_type":"Colombian","quantity":20,"order_date":"2021-03-25T07:00:00.000Z","subscription":2,"linked_customer_id":2},{"order_id":5,"coffee_type":"Indonesia","quantity":30,"order_date":"2021-03-25T07:00:00.000Z","subscription":9,"linked_customer_id":2},{"order_id":6,"coffee_type":"Mexico","quantity":35,"order_date":"2021-03-25T07:00:00.000Z","subscription":7,"linked_customer_id":3},{"order_id":7,"coffee_type":"Peru","quantity":40,"order_date":"2021-03-25T07:00:00.000Z","subscription":6,"linked_customer_id":3},{"order_id":8,"coffee_type":"Haitian","quantity":45,"order_date":"2021-03-25T07:00:00.000Z","subscription":12,"linked_customer_id":3}]
 
 
 describe('Order Router CRUD Routes', () => {
@@ -15,19 +14,124 @@ describe('Order Router CRUD Routes', () => {
     const res = await request(app)
       .get('/api/orders')
 
-    expect(res.body).toEqual(allOrdersResult)
-
-  });
-
-  it('Returns all orders from a single customer by ID', async () => {
-    const res = await request(app)
-      .get('/api/orders/customer/1')
-
     expect(res.body).toEqual([
-      {"order_id":1,"coffee_type":"Ethopian","quantity":5,"order_date":"2021-03-25T07:00:00.000Z","subscription":2,"linked_customer_id":1},
-      {"order_id":2,"coffee_type":"Honduras","quantity":10,"order_date":"2021-03-25T07:00:00.000Z","subscription":3,"linked_customer_id":1}
+      {
+          "orderId": 1,
+          "coffeeType": "Ethopian",
+          "quantity": 5,
+          "orderDate": "2021-03-25T07:00:00.000Z",
+          "subscription": 2,
+          "customerId": 1
+      },
+      {
+          "orderId": 2,
+          "coffeeType": "Honduras",
+          "quantity": 10,
+          "orderDate": "2021-03-25T07:00:00.000Z",
+          "subscription": 3,
+          "customerId": 1
+      },
+      {
+          "orderId": 3,
+          "coffeeType": "Brazil",
+          "quantity": 15,
+          "orderDate": "2021-03-25T07:00:00.000Z",
+          "subscription": 4,
+          "customerId": 2
+      },
+      {
+          "orderId": 4,
+          "coffeeType": "Colombian",
+          "quantity": 20,
+          "orderDate": "2021-03-25T07:00:00.000Z",
+          "subscription": 2,
+          "customerId": 2
+      },
+      {
+          "orderId": 5,
+          "coffeeType": "Indonesia",
+          "quantity": 30,
+          "orderDate": "2021-03-25T07:00:00.000Z",
+          "subscription": 9,
+          "customerId": 2
+      },
+      {
+          "orderId": 6,
+          "coffeeType": "Mexico",
+          "quantity": 35,
+          "orderDate": "2021-03-25T07:00:00.000Z",
+          "subscription": 7,
+          "customerId": 3
+      },
+      {
+          "orderId": 7,
+          "coffeeType": "Peru",
+          "quantity": 40,
+          "orderDate": "2021-03-25T07:00:00.000Z",
+          "subscription": 6,
+          "customerId": 3
+      },
+      {
+          "orderId": 8,
+          "coffeeType": "Haitian",
+          "quantity": 45,
+          "orderDate": "2021-03-25T07:00:00.000Z",
+          "subscription": 12,
+          "customerId": 3
+      }
     ])
 
   });
+  
+  it('Returns all orders from a single customer by ID', async () => {
+    const res = await request(app)
+    .get('/api/orders/customer/1')
+    
+    expect(res.body).toEqual([
+      {
+          "orderId": 1,
+          "coffeeType": "Ethopian",
+          "quantity": 5,
+          "orderDate": "2021-03-25T07:00:00.000Z",
+          "subscription": 2,
+          "customerId": 1
+      },
+      {
+          "orderId": 2,
+          "coffeeType": "Honduras",
+          "quantity": 10,
+          "orderDate": "2021-03-25T07:00:00.000Z",
+          "subscription": 3,
+          "customerId": 1
+      }
+    ])
+    
+  });
+  
+  it('POST: creates an order and returns the data', async () => {
+    const res = await request(app)
+      .post('/api/orders')
+      .send({
+        "name": "Some Name",
+        "address": "New Address",
+        "coffeeType": "Columbian", 
+        "quantity": 7, 
+        "subscription": 3
+      })
 
+    expect(res.body).toEqual({
+      "details": "order processed",
+      "data": {
+          "orderId": 9,
+          "coffeeType": "Columbian",
+          "quantity": 7,
+          "orderDate": "2021-03-25T07:00:00.000Z",
+          "subscription": 3,
+          "customerId": 4
+      }
+    })
+
+  });
+  
+  
 });
